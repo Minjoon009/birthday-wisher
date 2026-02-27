33e4ec12e07d65b3003b143f112638e8c576e400
Minjoon009 <132491546+Minjoon009@users.noreply.github.com>
2026-02-28 00:11:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import smtplib
 import pandas
 import datetime as dt
 import random
+import os
 birthday_data = pandas.read_csv("birthdays.csv").to_dict(orient="records")
 now = dt.datetime.now()
 now_month = now.month
 now_day = now.day
+my_email = os.environ["EMAIL"]
+password = os.environ["PASSWORD"]
 def get_random_letter(name):
     with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as letter_file:
         contents = letter_file.read()
         contents =  contents.replace("[NAME]",name)
     return contents
 def send_email(recipient_addr,message):
-    my_email = "aungheinhtetahhj2@gmail.com"
-    password = "rtwr pngn hzoh npgw"
     with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
         connection.login(user=my_email,password=password)
